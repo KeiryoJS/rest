4867dca77774cf15fa27c1ea1e831c387150ce16
melike2d <ML2DOfficial@gmail.com>
2021-04-25 13:08:43 -0700

    :fire: remove copyright header

--- src/constants.ts
+++ src/constants.ts
@@ -1,21 +1,3 @@
-/*
- * NeoCord
- * Copyright 2021 melike2d
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 const { homepage, version } = require("../package.json");
 
 export const IMAGE_SIZES = Array.from({ length: 9 }, (_e, i) => 2 ** (i + 4));
